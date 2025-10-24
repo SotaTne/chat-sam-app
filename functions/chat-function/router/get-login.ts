f4cbd26b81f09ef0ab4e9ddc7ad8c92185712779
sota <motarou.engineer@gmail.com>
2025-10-24 17:49:21 +0900

    不要なファイルの削除

--- functions/chat-function/router/get-login.ts
+++
@@ -1,6 +0,0 @@
-import { APIGatewayProxyResult } from "aws-lambda";
-import { handlerArgs } from "../config";
-
-export async function getLoginHandler(
-  args: handlerArgs
-): Promise<APIGatewayProxyResult> {}
